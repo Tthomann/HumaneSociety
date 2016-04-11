4d51ebfdf333995610368d411dd2a8d96ece0d57
Tyler Thomann <Tyler Thomann>
2016-04-11 07:34:18 -0500

    added sln

--- System.cs
+++ System.cs
@@ -22,7 +22,7 @@ namespace HumaneSociety
            
             while (!done) {
                 Console.WriteLine("--------------------------------------");
-                Console.WriteLine("press 1 to add a new dog, press 2 to let a person adopt a dog.");
+                Console.WriteLine("press 1 to add a new animal, press 2 to let a person adopt a animal.");
                 Console.WriteLine("press 3 to check if the animal has had their shot, or press 4 to administer a shot to the animal.");
                 Console.WriteLine("press 5 to check if the animal has been fed today, or press 6 to give an animal some food.");
                 Console.WriteLine("press 7 to print out the current animals in the shelter.");
